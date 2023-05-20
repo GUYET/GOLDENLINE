9907da6c83000a51f7c1cd07075a6a0caadd7ce9
Khaty GUYET <k.guyet@gmail.com>
2023-05-20 12:01:41 +0200

    create admin user

--- app_collect/admin.py
+++ app_collect/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RequeteAnonymisees, RequeteCollecte
+
 
 # Register your models here.
+
+admin.site.register(RequeteAnonymisees)
+admin.site.register(RequeteCollecte)
