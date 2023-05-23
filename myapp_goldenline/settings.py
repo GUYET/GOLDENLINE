075e14ff3dbd89e51773397fcd3088c10a1d38e8
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 14:18:14 +0200

    update db

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "appcollect",
+    "dj_database_url",
     "whitenoise",
 ]
 
@@ -85,6 +87,8 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ["DATABASE_URL"]
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
