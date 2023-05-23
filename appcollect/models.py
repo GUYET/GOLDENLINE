ccef92e08cb7f52b94a7436b819eace71eef2b0f
Khaty Guyet <95047548+GUYET@users.noreply.github.com>
2023-05-23 19:21:42 +0200

    Update models.py

--- appcollect/models.py
+++ appcollect/models.py
@@ -68,3 +68,13 @@ class DataPanierMoyenCsp(models.Model):
     class Meta:
         managed = False
         db_table = "depense_panier_moyen_csp"
+        
+   from django.db import models
+
+
+class Editors(models.Model):
+    editor_name = models.CharField(max_length=200)
+    num_users = models.IntegerField()
+
+    def __str__(self):
+        return "{}-{}".format(self.editor_name, self.num_users) 
