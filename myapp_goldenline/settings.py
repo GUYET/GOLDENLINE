3b5c9605a5a8ff26bb170afa9db49fc5ac386214
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 09:20:33 +0200

    update database

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = "myapp_goldenline.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 if DEBUG:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postrgres.sql",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "appcollect",
+            "USER": "postgres",
+            "PASSWORD": "($*lsfLxhBuqLp$",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
         },
         "test": {
             "ENGINE": "django.db.backends.sqlite3",
