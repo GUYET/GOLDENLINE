de367072b07f32988699f82e9f8f7c09633177fe
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 15:48:43 +0200

    update db

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
 
 SECRET_KEY = os.getenv(
     "SECRET_KEY", "django-insecure-c-kmul0r7ryw07p#v1!glo7t83b5r_9^0dj3@saw0z-&!kce4+"
