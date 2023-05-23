ac643954b1add8d7b8020c05bebe966dfe195d37
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 16:28:54 +0200

    update debug

--- myapp_goldenline/prod_settings.py
+++ myapp_goldenline/prod_settings.py
@@ -1,8 +1,8 @@
 from .settings import *
 import dj_database_url
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ["goldenline-project.herokuapp.com"]
 SECRET_KEY = "toa5+@)s8@(dyn7m5^m#p8rglrk@d)c09+p(d6u9wepzg8a57%"
 
