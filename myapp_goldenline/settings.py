b5423a8724639e32fc6248eeec2a298a768a975c
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 10:53:49 +0200

    update

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-c-kmul0r7ryw07p#v1!glo7t83b5r_9^0dj3@saw0z-&!kce4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["goldenline-project.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
