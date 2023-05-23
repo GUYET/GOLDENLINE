cbdcd0188fb07106c0cc0c0c14842def9470a6ee
Khaty GUYET <k.guyet@gmail.com>
2023-05-23 12:32:15 +0200

    update

--- myapp_goldenline/prod_settings.py
+++ myapp_goldenline/prod_settings.py
@@ -1,11 +1,6 @@
 from .settings import *
-import dj_database_url
-
 
 DEBUG = True
 TEMPLATE_DEBUG = True
 LLOWED_HOSTS = ["goldenline-project.herokuapp.com"]
 SECRET_KEY = "toa5+@)s8@(dyn7m5^m#p8rglrk@d)c09+p(d6u9wepzg8a57%"
-
-
-DATABASES = ["default"] = dj_database_url.config()
--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = "myapp_goldenline.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": BASE_DIR / "db.postgres.sql",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+}
 
 
 # Password validation
