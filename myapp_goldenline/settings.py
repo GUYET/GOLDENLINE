04958b91e8065f78ec436b24a03a4b19e75a5848
Khaty Guyet <95047548+GUYET@users.noreply.github.com>
2023-05-24 15:39:41 +0200

    Update settings.py

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = "myapp_goldenline.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgres",
+        "NAME": BASE_DIR / "db.postgres.sql",
     }
 }
 
