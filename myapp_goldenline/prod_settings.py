11b1dd7916ac40b7020e2930dc08b9892d267bac
Khaty Guyet <95047548+GUYET@users.noreply.github.com>
2023-05-24 16:46:24 +0200

    Update prod_settings.py

--- myapp_goldenline/prod_settings.py
+++ myapp_goldenline/prod_settings.py
@@ -5,6 +5,5 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ["goldenline-project.herokuapp.com"]
 
-SECRET_KEY = "toa5+@)s8@(dyn7m5^m#p8rglrk@d)c09+p(d6u9wepzg8a57%"
 
 DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
