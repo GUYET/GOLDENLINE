8154a64fd5a5ede3163538d3252aadb2bb8594ee
Khaty Guyet <95047548+GUYET@users.noreply.github.com>
2023-05-26 11:40:19 +0200

    Update settings.py

--- myapp_goldenline/settings.py
+++ myapp_goldenline/settings.py
@@ -151,4 +151,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = ""
-LOGOUT_REDIRECT_URL = "/login"
+LOGOUT_REDIRECT_URL = ""
