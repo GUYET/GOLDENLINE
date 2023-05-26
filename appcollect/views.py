ef2d66b22db13d5b90d6538ed2a9cbd32c8845f5
Khaty Guyet <95047548+GUYET@users.noreply.github.com>
2023-05-26 11:04:59 +0200

    Update views.py

--- appcollect/views.py
+++ appcollect/views.py
@@ -12,6 +12,23 @@ from .models import (
 
 
 #Création des views
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            # Authentification réussie, connectez l'utilisateur
+            login(request, user)
+            return redirect('')  # Redirigez vers la page d'accueil ou une autre page
+        else:
+            # Authentification échouée, affichez un message d'erreur
+            error_message = "Nom d'utilisateur ou mot de passe incorrect."
+            return render(request, 'login.html', {'error_message': error_message})
+    
+    return render(request, 'login.html')
 
 def visualisation(request):
     requetes_anonymisees = RequeteAnonymisees.objects.all()
